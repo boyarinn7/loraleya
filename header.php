40095b3bbb6751ea958817f049570fc09a64a015
boyarinn7 <boyarinn7@yandex.ru>
2026-05-02 04:42:45 +0300

    feat(nav): active menu item via PHP conditions, remove Catalog link
    Hardcoded nav now detects current page via is_singular/is_tax/is_page
    and adds .current-menu-item class. CSS rules already in place from
    previous commit. Catalog link removed.

--- header.php
+++ header.php
@@ -14,12 +14,17 @@
         Lora<em>Leya</em>
     </a>
 
+    <?php
+    $is_scenario      = is_singular('scenario') || is_post_type_archive('scenario');
+    $is_palette       = is_tax('pa_fabric_color');
+    $is_custom_order  = is_page('custom-order');
+    $is_about         = is_page('about');
+    ?>
     <nav class="main-nav" role="navigation">
-        <a href="<?php echo home_url('/#scenarios'); ?>">Сценарии</a>
-        <a href="<?php echo home_url('/#palette'); ?>">Палитра</a>
-        <a href="<?php echo get_permalink(wc_get_page_id('shop')); ?>">Каталог</a>
-        <a href="<?php echo home_url('/custom-order/'); ?>">Индивидуальный заказ</a>
-        <a href="<?php echo home_url('/about/'); ?>">О бренде</a>
+        <a href="<?php echo home_url('/#scenarios'); ?>" class="<?php echo $is_scenario ? 'current-menu-item' : ''; ?>">Сценарии</a>
+        <a href="<?php echo home_url('/#palette'); ?>" class="<?php echo $is_palette ? 'current-menu-item' : ''; ?>">Палитра</a>
+        <a href="<?php echo home_url('/custom-order/'); ?>" class="<?php echo $is_custom_order ? 'current-menu-item' : ''; ?>">Индивидуальный заказ</a>
+        <a href="<?php echo home_url('/about/'); ?>" class="<?php echo $is_about ? 'current-menu-item' : ''; ?>">О бренде</a>
     </nav>
 
     <a href="<?php echo wc_get_cart_url(); ?>" class="header-cart">
